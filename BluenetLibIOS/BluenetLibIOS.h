1195cac517587df413c48372aeceb71649608e7f
Alex de Mulder <alexdemulder@gmail.com>
2016-04-11 15:13:17 +0200

    Initial Commit

---
+++ BluenetLibIOS/BluenetLibIOS.h
@@ -0,0 +1,19 @@
+//
+//  BluenetLibIOS.h
+//  BluenetLibIOS
+//
+//  Created by Alex de Mulder on 11/04/16.
+//  Copyright © 2016 Alex de Mulder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BluenetLibIOS.
+FOUNDATION_EXPORT double BluenetLibIOSVersionNumber;
+
+//! Project version string for BluenetLibIOS.
+FOUNDATION_EXPORT const unsigned char BluenetLibIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluenetLibIOS/PublicHeader.h>
+
+
