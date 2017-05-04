bc8f0d24107c79144ade8b4252f361ef551a22ba
Alex de Mulder <alexdemulder@gmail.com>
2017-05-04 11:02:04 +0200

    added hardware version info

--- BluenetLib/backend/serviceImplementations/DeviceHandler.swift
+++ BluenetLib/backend/serviceImplementations/DeviceHandler.swift
@@ -35,4 +35,34 @@ open class DeviceHandler {
             .then{ data -> Promise<String> in return Promise<String>{fulfill, reject in fulfill(Conversion.uint8_array_to_string(data)) }}
     }
     
+    /**
+     * Returns a hardware version:
+     *  hardwareVersion + productionRun + housingId + reserved + nordicChipVersion
+     *
+     *  hardwareVersion:
+     *  ----------------------
+     *  | GENERAL | PCB      |
+     *  | PRODUCT | VERSION  |
+     *  | INFO    |          |
+     *  ----------------------
+     *  | 1 01 02 | 00 92 00 |
+     *  ----------------------
+     *  |  |  |    |  |  |---  Patch number of PCB version
+     *  |  |  |    |  |------  Minor number of PCB version
+     *  |  |  |    |---------  Major number of PCB version
+     *  |  |  |--------------  Product Type: 1 Dev, 2 Plug, 3 Builtin, 4 Guidestone
+     *  |  |-----------------  Market: 1 EU, 2 US
+     *  |--------------------  Family: 1 Crownstone
+     *
+     * productionRun = "0000"         (4)
+     * housingId = "0000"             (4)
+     * reserved = "00000000"          (8)
+     * nordicChipVersion = "xxxxxx"   (6)
+     */
+    open func getHardwareRevision() -> Promise<String> {
+        return self.bleManager.readCharacteristicWithoutEncryption(CSServices.DeviceInformation, characteristic: DeviceCharacteristics.HardwareRevision)
+            .then{ data -> Promise<String> in return Promise<String>{fulfill, reject in fulfill(Conversion.uint8_array_to_string(data)) }}
+    }
+
+    
 }
