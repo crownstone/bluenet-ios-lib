31f1b90e8c188d4ed9dd5de30ff9bb00fc7078d1
alexdm0 <alexdemulder@gmail.com>
2021-08-09 12:16:56 +0200

    Removed project

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(
             name:"BluenetShared",
             url: "https://github.com/crownstone/bluenet-ios-shared",
-            .exact("1.2.0")
+            .exact("1.2.1")
         ),
         .package(
             url: "https://github.com/krzyzanowskim/CryptoSwift",
